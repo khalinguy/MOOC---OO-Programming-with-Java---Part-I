5c089a6a5cee870db5c870aa1bb33dea70427b4f
Khanh Linh Nguyen <58554649+khalinguy@users.noreply.github.com>
2019-12-06 02:14:31 +0200

    Add files via upload

---
+++ week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,14 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 999999999;
+        int b = 999999999;
+        int result = a * b;
+        
+        
+        System.out.print(a + " * " + b + " = " + result);
+        // Program your solution here. Remember to use variables a and b!
+    }
+
+}
