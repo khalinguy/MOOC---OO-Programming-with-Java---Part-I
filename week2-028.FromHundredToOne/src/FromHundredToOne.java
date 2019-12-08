ed20657a64e1b112f6998854c4ab4f70e74b9ccb
Khanh Linh Nguyen <58554649+khalinguy@users.noreply.github.com>
2019-12-09 00:44:19 +0200

    Add files via upload

---
+++ week2-028.FromHundredToOne/src/FromHundredToOne.java
@@ -0,0 +1,12 @@
+
+public class FromHundredToOne {
+
+    public static void main(String[] args) {
+        int nume = 100;
+        while (nume>0) {
+            System.out.println(nume);
+            nume --;
+        }
+        // Write your program here
+    }
+}
