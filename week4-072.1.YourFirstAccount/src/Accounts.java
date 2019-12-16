fc59ca54d2390c9228cdde3fd55c8d362e932469
Khanh Linh Nguyen <58554649+khalinguy@users.noreply.github.com>
2019-12-17 01:14:13 +0200

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        
+        Account myAccount = new Account("Linh", 100.0);
+        myAccount.deposit(20.0);
+        System.out.println(myAccount);
+        // Code in Account.Java should not be touched!
+        // write your code here
+    }
+
+}
