ad628e0b474454ee265d25990e00938f420d1183
Khanh Linh Nguyen <58554649+khalinguy@users.noreply.github.com>
2019-12-19 01:48:12 +0200

    Add files via upload

---
+++ week5-091.ExtendingMyDate/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test your code here
+        MyDate day = new MyDate(25, 2, 2011);
+        day.advance(7);
+        System.out.println(day);
+    }
+}
